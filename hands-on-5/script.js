d574f8099b81d4115ed83ae041bfce80006c85db
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-09-21 23:23:09 +0700

    update hands-on-5 and add example hands-on-5

--- hands-on-5/script.js
+++ hands-on-5/script.js
@@ -29,6 +29,16 @@ const main = () => {
   const deleteTask = document.getElementById("task-item");
   deleteTask.addEventListener();
   deleteTask.remove;
+
+  var myNodelist = document.getElementsByTagName("LI");
+  var i;
+  for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+  }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
